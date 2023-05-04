53f3ea4f2905959c49ddce7c259a94dbd617454a
Swadhin Ghosh <43653792+Swadhincse@users.noreply.github.com>
2023-05-05 01:28:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// get the element with id "level"
+const levelElement = document.getElementById("level");
+
+// get the number of parent elements of the level element
+const parentCount = levelElement.parentsUntil("body").length;
+
+// display the result in an alert box
+alert("The level of the element is: " + (parentCount + 1));
